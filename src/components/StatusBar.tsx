e40ff2be7680f57da317f947983431b329cf3301
Sebastian Behnisch <sebastian.behnisch@siemens-energy.com>
2023-04-04 16:02:58 +0200

    first price indictor added

---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,9 @@
+export function StatusBar(): JSX.Element {
+    return (
+        <>
+            <div>
+                <p>Status Display with general numbers Price DFI:</p>
+            </div>
+        </>
+    )
+}
