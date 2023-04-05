d1f42a026254a9e285319f15e92ad57f9b248464
Sebastian Behnisch <sebastian.behnisch@siemens-energy.com>
2023-04-05 15:39:34 +0200

    status bar item added

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,8 +1,17 @@
-export function StatusBar(): JSX.Element {
+import { StatsData } from "@defichain/whale-api-client/dist/api/stats";
+
+interface StatusBarI {
+    stats: StatsData
+}
+
+export function StatusBar(props: StatusBarI): JSX.Element {
     return (
         <>
             <div>
-                <p>Status Display with general numbers Price DFI:</p>
+                <p>DFI: {props.stats.price.usd}</p>
+                <p>Blocks: {props.stats.count.blocks}</p>
+                <p>Blockrewards: {props.stats.emission.total}</p>
+                <p>TVL: {props.stats.tvl.total}</p>
             </div>
         </>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import { StatusBar } from '@/components/StatusBar'
-import { GetServerSidePropsContext, GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
+import {GetServerSidePropsResult, InferGetServerSidePropsType } from 'next'
 import { Block } from "@defichain/whale-api-client/dist/api/blocks";
 import { PoolPairData } from "@defichain/whale-api-client/dist/api/poolpairs";
 import { Transaction } from "@defichain/whale-api-client/dist/api/transactions";
-import { StatsData, SupplyData } from "@defichain/whale-api-client/dist/api/stats";
+import { StatsData } from "@defichain/whale-api-client/dist/api/stats";
 import { WhaleApiClient } from "@defichain/whale-api-client";
 import { MainNet, Network, TestNet } from '@defichain/jellyfish-network'
+import { useEffect, useState } from 'react';
 
 interface HomePageProps{
   blocks: Block[]
@@ -18,6 +19,46 @@ interface HomePageProps{
 export default function Home(
   props: InferGetServerSidePropsType<typeof getServerSideProps>
 ): JSX.Element {
+  const client = new WhaleApiClient({
+    url: 'https://ocean.defichain.com',
+    version: 'v0',
+    network: MainNet.name
+  })
+  const [data, setData] = useState<HomePageProps>({
+    blocks: props.blocks,
+    stats: props.stats,
+    transactions: props.transactions,
+    liquidityPools: props.liquidityPools,
+  });
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetchData()
+    },5000)
+    return () => clearInterval(intervalId)
+  })
+
+  async function fetchData(): Promise<void>{
+    const blocks = await client.blocks.list(8)
+    const stats = await client.stats.get()  
+    let transactions: Transaction[] = []
+    await Promise.all(
+      blocks.map(async (block) => 
+        await client.blocks.getTransactions(block.id,8).then((results) => {
+          return results
+        })
+      )
+    ).then((results) => {
+      results.map((result) => transactions.push(...result))
+    })
+    transactions = transactions.slice(0,8)
+    const liquidityPools = await client.poolpairs.list(8)
+    setData({
+      blocks,
+      stats,
+      transactions,
+      liquidityPools
+    })
+  }
   return (
     <>
       <Head>
@@ -28,8 +69,8 @@ export default function Home(
       </Head>
       <main>
         <div>
-          <StatusBar />
-          <p>Hier geht es weiter Price of DFI: { props.stats.price.usd }</p>
+          <StatusBar stats={data.stats} />
+          <p>Hier geht es weiter Price of DFI: { data.stats.price.usd }</p>
         </div>
       </main>
     </>
@@ -44,7 +85,6 @@ export async function getServerSideProps(): Promise<GetServerSidePropsResult<Hom
   })
   const blocks = await client.blocks.list(8)
   const stats = await client.stats.get()  
-
   let transactions: Transaction[] = []
   await Promise.all(
     blocks.map(async (block) => 
@@ -56,9 +96,7 @@ export async function getServerSideProps(): Promise<GetServerSidePropsResult<Hom
     results.map((result) => transactions.push(...result))
   })
   transactions = transactions.slice(0,8)
-
   const liquidityPools = await client.poolpairs.list(8)
-
   return {
     props: {
       blocks,
